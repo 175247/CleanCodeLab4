4cdca93d89f6d734f479c38539ed09bd9d06c592
175247 <yarr-tm@hotmail.com>
2021-01-05 22:12:09 +0100

    Add addition-calculation tests and endpoint.

---
+++ AdditionCalculationTests/AdditionCalculationTests.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab4AdditionCalculationService.Controllers;
+
+namespace AdditionCalculationTests
+{
+    [TestClass]
+    public class AdditionCalculationTests
+    {
+        private readonly AdditionCalculationsController _calculator;
+
+        public AdditionCalculationTests()
+        {
+            _calculator = new AdditionCalculationsController();
+        }
+
+        [TestMethod]
+        public void adding_two_numbers_together_should_return_correct_amount_when_called()
+        {
+            // Arrange
+            int firstNumber = 3;
+            int secondNumber = 4;
+            var expected = 7;
+
+            // Act
+            var actual = _calculator.Add(firstNumber, secondNumber);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Lab4AdditionCalculationService/Controllers/AdditionCalculationsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4AdditionCalculationService.Controllers
+{
+    public class AdditionCalculationsController : Controller
+    {
+        [HttpGet]
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
